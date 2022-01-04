2065e60e986ac36ab244629abd35b35290fb52dd
Adam Williams <Adam24Williams@gmail.com>
2022-01-04 08:23:39 -0500

    Added scripts

---
+++ appCheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Written By: Adam Williams
+# Info: Check to see if a specific app is not installed. If it is, update inventory. If it is not, run custom policy event.
+
+# Paramater 4: Application Name
+# Paramater 5: Custom Event Name
+
+if [[ $4 == "" || $5 == "" ]]; then
+	echo "One or both paramaters are missing"
+    exit 1
+fi
+
+if [[ -d "/Applications/$4.app" ]]; then
+	echo "$4 is installed. Updating inventory."
+    jamf recon
+else
+	echo "$4 is not installed. Attempting to install."
+    jamf policy -event $5
+fi
+
+exit 0
